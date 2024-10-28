66ab2be0f9bf32c2c8490a928ce2176a052b6f6b
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-10-28 17:17:49 +0100

    Tareas terminadas y subidas

---
+++ UD3/05_dom/main.js
@@ -0,0 +1,12 @@
+const allDivs = document.querySelectorAll('div');
+console.log(allDivs);
+
+const elementsWithStyleButtons = document.querySelectorAll('[style*="buttons"]');
+console.log(elementsWithStyleButtons);
+
+const oneDivWithButtonsClass = document.querySelector('buttons');
+console.log(oneDivWithButtonsClass);
+
+const allDivsWithButtonsClass = document.querySelectorAll('div.buttons');
+console.log(allDivsWithButtonsClass);
+
